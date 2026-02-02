70c9be8cf0c86e71f176b32b73e073b9a2818ca0
Xandev-4 <gyanachary7@gmail.com>
2026-02-02 16:23:33 +0530

    added markdown support, added nvim surround and some minor changes

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("core.lazy")
 require("core.keymaps")
 require("core.options")
+require("fzf-lua").register_ui_select()
 
 local transparent_groups = {
 	"Normal",
@@ -19,3 +20,6 @@ local transparent_groups = {
 for _, group in ipairs(transparent_groups) do
 	vim.cmd(string.format("hi %s guibg=none ctermbg=none", group))
 end
+
+vim.opt.conceallevel = 2
+vim.api.nvim_set_hl(0, "@markup.strong", { bold = true })
--- lua/plugins/treesitter-nvim.lua
+++ lua/plugins/treesitter-nvim.lua
@@ -1,22 +1,43 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {"lua", "vim", "vimdoc", "c", "cpp", "git_config", "html", "css", "java", "javascript", "json", "luadoc", "python", "php", "rust", "sql", "typescript", "yaml"},
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-          node_incremental = "<Enter>",
-          scope_incremental = false,
-          node_decremental = "<Backspace>",
-        },
-      },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"c",
+				"cpp",
+				"git_config",
+				"html",
+				"css",
+				"java",
+				"javascript",
+				"json",
+				"luadoc",
+				"python",
+				"php",
+				"rust",
+				"sql",
+				"typescript",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
+		})
+	end,
 }
