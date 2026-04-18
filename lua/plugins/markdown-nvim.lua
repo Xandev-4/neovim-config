8f8132ff7918878e68168fafe616324a4acbf86b
Xandev-4 <gyanachary7@gmail.com>
2026-04-18 18:32:27 +0530

    updated treesitter for its new config

--- lua/plugins/markdown-nvim.lua
+++ lua/plugins/markdown-nvim.lua
@@ -5,5 +5,11 @@ return {
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		-- pipe_table = {
+		-- 	border_virtual = true,
+		-- 	cell = "overlay",
+		-- },
+		-- latex = { enable = false },
+	},
 }
--- lua/plugins/treesitter-nvim.lua
+++ lua/plugins/treesitter-nvim.lua
@@ -1,10 +1,11 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			-- Replaces ensure_installed (idempotent, skips already-installed parsers)
+			require("nvim-treesitter").install({
 				"lua",
 				"vim",
 				"vimdoc",
@@ -25,19 +26,37 @@ return {
 				"yaml",
 				"markdown",
 				"markdown_inline",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
+			})
+
+			-- Replaces highlight = { enable = true } and indent = { enable = true }
+			-- Main branch no longer auto-enables these; you wire them manually
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true }),
+				callback = function(args)
+					-- pcall so it silently skips filetypes with no parser
+					local ok = pcall(vim.treesitter.start, args.buf)
+					if ok then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
+	},
+
+	-- Incremental selection is gone from main, this plugin brings it back
+	{
+		"MeanderingProgrammer/treesitter-modules.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+					init_selection = "<Enter>",
 					node_incremental = "<Enter>",
 					scope_incremental = false,
 					node_decremental = "<Backspace>",
 				},
 			},
-		})
-	end,
+		},
+	},
 }
